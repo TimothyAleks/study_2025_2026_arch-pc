bca21457143cc010db13ff7ef7e6b0016e9843f5
TimothyAleks <tima.alekseev2109@gmail.com>
2025-11-23 11:20:32 +0300

    Add files via upload

---
+++ labs/lab07/lab7-1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("Сообщение №3\n");
+    printf("Сообщение №2\n");
+    printf("Сообщение №1\n");
+    return 0;
+}
---
+++ labs/lab07/lab7-2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int B = 98;
+    int maxNumber = 15;
+
+    // Выводим запрос с значением B
+    printf("Введите B: %d\n", B);
+
+    // Выводим строку с наибольшим числом
+    printf("Наибольшее число: %d\n", maxNumber);
+
+    return 0;
+}
